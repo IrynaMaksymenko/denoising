552007e0bb26ebf88245f50e7c41c37bbe365a2c
IrynaMaksymenko <maksymenko.i.o@gmail.com>
2016-05-03 20:59:26 +0300

    made only one input for nn

--- nn/load_train_data.m
+++ nn/load_train_data.m
@@ -1,49 +1,59 @@
-function [x, y, length] = load_train_data()
-train_images_directory = 'D:\Ira\ML Project\denoising\data\train_tiny';
-train_cleaned_images_directory = 'D:\Ira\ML Project\denoising\data\train_cleaned_tiny';
+function [x, y, block_size] = load_train_data()
+train_images_directory = 'D:\Ira\ML Project\denoising\data\train_258';
+train_cleaned_images_directory = 'D:\Ira\ML Project\denoising\data\train_cleaned_258';
 
 input_images = dir(sprintf('%s/*.png', train_images_directory));
-%s = length(input_images(not([input_images.isdir])));
+image_count = length(input_images(not([input_images.isdir])));
+rows = 258;
+columns = 540;
+
 block_s1 = 43;
 block_s2 = 45;
-x = cell(72, 1);
-y = cell(72, 1);
+block_size = block_s1*block_s2;
+
+blocks_in_one_image = (rows/block_s1)*(columns/block_s2);
+
+data_size = blocks_in_one_image * image_count;
+
+x = zeros(block_size, data_size);
+y = zeros(block_size, data_size);
+
+image_index = 0;
 for img = input_images'
     
     imdata = double(imread(sprintf('%s/%s', train_images_directory, img.name)));
-    [rows, columns] = size(imdata);
-    
+        
     % split image into blocks
     blocks = reshape(imdata, [block_s1, rows/block_s1, block_s2, columns/block_s2]);
     [~, s2, ~, s4] = size(blocks);
     
-    index = 1;
+    index = image_index*blocks_in_one_image+1;
     for row = 1 : s2
         for column = 1 : s4            
             block = blocks(:, row, :, column);
 
             feature_vector = create_feature_vector(block);
-            x{index, 1} = feature_vector;
+            x(:, index) = feature_vector;
             index = index + 1;
         end;
     end;
     
 
     target_imdata = double(imread(sprintf('%s/%s', train_cleaned_images_directory, img.name)));
+    
     target_blocks = reshape(target_imdata, [block_s1, rows/block_s1, block_s2, columns/block_s2]);
     
-    index = 1;
+    index = image_index*blocks_in_one_image+1;
     for row = 1 : s2
         for column = 1 : s4            
             target_block = target_blocks(:, row, :, column);
             
             target_vector = create_target_vector(target_block);
-            y{index, 1} = target_vector;
+            y(:, index) = target_vector;
             index = index + 1;
         end;
     end;
     
-    length = rows/block_s1 * columns/block_s2;
-
+    image_index = image_index + 1;
 end
 end
--- nn/train_nn.m
+++ nn/train_nn.m
@@ -1,7 +1,7 @@
 
 %-----------------------------------------------------------------------
 % Loading data
-[x, y, length] = load_train_data();
+[x, y, data_size] = load_train_data();
 
 %------------------------------------------------------------------------
 % Creation of NN
@@ -9,45 +9,38 @@
 net = network;
 
 % inputs are blocks(parts) of images
-net.numInputs = length;
+net.numInputs = 1;
 
 % each block represents image window of size 43*45
-for i = 1:length
-    net.inputs{i}.size = 43*45;
-end;
+% inputs size is the number of blocks
+net.inputs{1}.size = data_size;
 
 % there is one hidden layer and one output layer for each output
-net.numLayers = length + 1;
+net.numLayers = 2;
 
 % on hidden layer there are 50 neurons
-net.layers{1}.size = 10;
+net.layers{1}.size = 50;
 net.layers{1}.transferFcn = 'logsig';
 net.layers{1}.initFcn = 'initnw';
 
 % output layers
-for lay = 2:length+1
-    net.layers{lay}.size = 43*45;
+for lay = 2:2
+    net.layers{lay}.size = data_size;
     net.layers{lay}.transferFcn = 'purelin';
     net.layers{lay}.initFcn = 'initnw';
 end;
 
 % bias is on each layer
-net.biasConnect = ones(length+1, 1);
+net.biasConnect = ones(2, 1);
 
 % input is connected only to first layer
-for i = 1:length
-    net.inputConnect(1,i) = 1;
-end;
+net.inputConnect(1,1) = 1;
 
 % first layer is connected to all output layers
-for i = 2:length+1
-    net.layerConnect(i,1) = 1;
-end;
+net.layerConnect(2,1) = 1;
 
 % output layers are connected with output
-for i = 2:length+1
-    net.outputConnect(i) = 1;
-end;
+net.outputConnect(2) = 1;
 
 % training parameters
 net.initFcn = 'initlay';
